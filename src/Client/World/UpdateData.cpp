c00ad9aae3f580a534eea272e4c9e0b28cd3b392
False.Genesis <False.Genesis@fda9a0ab-4e26-0410-ba80-894f06a4b443>
2007-02-03 01:41:42 +0000

    * added some more SMSG_(COMPRESSED_)UPDATE_OBJECT stuff. * we have year 2007 now

--- src/Client/World/UpdateData.cpp
+++ src/Client/World/UpdateData.cpp
@@ -1,11 +1,15 @@
 #include "common.h"
 #include "ZCompressor.h"
 #include "WorldSession.h"
+#include "Object.h"
 #include "UpdateData.h"
+#include "UpdateFields.h"
+#include "UpdateMask.h"
+
 
 void WorldSession::_HandleCompressedUpdateObjectOpcode(WorldPacket& recvPacket)
 {
-    printf("-> COMPRESSED_UPDATE_OBJECT, pktlen=%u\n",recvPacket.size());
+    //printf("-> COMPRESSED_UPDATE_OBJECT, pktlen=%u\n",recvPacket.size());
     //recvPacket.hexlike();
     uint32 realsize;
     recvPacket >> realsize;
@@ -14,10 +18,9 @@ void WorldSession::_HandleCompressedUpdateObjectOpcode(WorldPacket& recvPacket)
     z.Compressed(true);
     z.RealSize(realsize);
     z.Inflate();
-    printf("-> Uncompressed to %u bytes\n",z.size());
     if(z.Compressed())
     {
-        log("ERROR: _HandleCompressedUpdateObjectOpcode(): Inflate() failed!");
+        logerror("_HandleCompressedUpdateObjectOpcode(): Inflate() failed!");
         return;
     }
     WorldPacket wp;
@@ -29,24 +32,70 @@ void WorldSession::_HandleCompressedUpdateObjectOpcode(WorldPacket& recvPacket)
 
 void WorldSession::_HandleUpdateObjectOpcode(WorldPacket& recvPacket)
 {
-     printf("-> UPDATE_OBJECT, pktlen=%u\n",recvPacket.size());
      //recvPacket.hexlike();
      uint8 utype;
-     uint32 usize;
+     uint8 unk8;
+     uint32 usize, ublocks;
      uint64 uguid;
-     //while(true)
+     recvPacket >> ublocks >> unk8;
+     logdebug("UpdateObject: ublocks=%u unk=%u",ublocks,unk8);
+     //while(true) // need to read full packet as soon as the structure is 100% known & implemented
+                   // for now reading first object is enough
      {
-         recvPacket >> utype >> usize;
+         recvPacket >> utype;
+         logdebug("UpdateObject: utype=%u",utype);
          switch(utype)
          {
             case UPDATETYPE_OUT_OF_RANGE_OBJECTS:
-            for(uint16 i=0;i<usize;i++)
+                recvPacket >> usize;
+                for(uint16 i=0;i<usize;i++)
+                {
+                    uguid = recvPacket.GetPackedGuid(); // not 100% sure if this is correct
+                    logdebug("GUID "I64FMT" out of range",uguid);
+                    // TODO: delete object from known objects list
+                }
+            break;
+
+            case UPDATETYPE_VALUES:
             {
-                recvPacket >> uguid;
-                printf("DEBUG: GUID "I64FMT" out of range\n",uguid);
-                // TODO: delete object from known objects list
+                uguid = recvPacket.GetPackedGuid(); // not 100% sure if this is correct
+                uint8 maskblocks;
+                recvPacket >> maskblocks;
+                logdebug("UPDATETYPE_VALUES: guid="I64FMT" maskblocks=%u",uguid,maskblocks);
+                // need some help with the lines below, got no idea what to do now.
+                UpdateMask umask;
+                umask.SetCount(maskblocks*8); // ??
+                std::vector<uint8> udata;
+                //udata.resize(?)
+
+
             }
             break;
+
+            case UPDATETYPE_CREATE_OBJECT:
+            case UPDATETYPE_CREATE_OBJECT2:
+            {
+                uguid = recvPacket.GetPackedGuid();
+                uint8 objtypeid;
+                recvPacket >> objtypeid;
+                logdebug("Create Object type %u with guid "I64FMT,objtypeid,uguid);
+                if(objtypeid==TYPEID_PLAYER)
+                {
+
+                }
+                if(objtypeid==TYPEID_UNIT)
+                {
+
+                }
+                if( (objtypeid==TYPEID_CORPSE) || (objtypeid==TYPEID_GAMEOBJECT) || (objtypeid==TYPEID_DYNAMICOBJECT))
+                {
+
+                }
+
+                // (TODO) and then: Add object to objmgr
+            }
+            break;
+
 
             default:
                 break;
