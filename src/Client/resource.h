3cba5a1e838ba2a076e0402ec3b295e087d81ec2
False.Genesis <False.Genesis@fda9a0ab-4e26-0410-ba80-894f06a4b443>
2007-02-09 17:04:11 +0000

    * deleted some old obsoelete files * fixed problems with afxres.h in resources, thx to bLuma

--- src/Client/resource.h
+++ src/Client/resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
-// Used by Script1.rc
+// Microsoft Visual C++ generated include file.
+// Used by PseuWoW.rc
 //
 #define IDI_ICON1                       101
 
@@ -8,9 +8,9 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        104
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
