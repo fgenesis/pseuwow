6b846612f3673247b2f98a8c11266cd6bdbebba8
False.Genesis <False.Genesis@fda9a0ab-4e26-0410-ba80-894f06a4b443>
2007-03-24 15:19:19 +0000

    * alpha 13 finished. more bugfix versions might follow, if any major bugs occur.
    git-svn-id: https://pseuwow.svn.sourceforge.net/svnroot/pseuwow@104 fda9a0ab-4e26-0410-ba80-894f06a4b443

--- src/Client/PseuWoW.cpp
+++ src/Client/PseuWoW.cpp
@@ -46,8 +46,8 @@ void PseuInstanceRunnable::sleep(uint32 msecs)
 PseuInstance::PseuInstance(PseuInstanceRunnable *run)
 {
     _runnable=run;
-    _ver="PseuWoW Alpha Build 13 development version 1" DEBUG_APPENDIX;
-    _ver_short="A13-dev1" DEBUG_APPENDIX;
+    _ver="PseuWoW Alpha Build 13" DEBUG_APPENDIX;
+    _ver_short="A13" DEBUG_APPENDIX;
     _wsession=NULL;
     _rsession=NULL;
     _scp=NULL;
