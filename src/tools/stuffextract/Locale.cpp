25c2f09525cbbb633e401938d7440a962de222b4
False.Genesis <False.Genesis@fda9a0ab-4e26-0410-ba80-894f06a4b443>
2007-06-13 13:29:19 +0000

    * stuffextract: added autodetection of currently used locale * fixed wrongly interpreted float fields in DBCs

--- src/tools/stuffextract/Locale.cpp
+++ src/tools/stuffextract/Locale.cpp
@@ -1,13 +1,56 @@
+#include "common.h"
 #include "Locale.h"
+#include <fstream>
+#include "ByteBuffer.h"
 
-char *my_locale;
+char my_locale[5];
+char *cconf = "WTF/config.wtf";
+char *cconfentry = "SET locale \"";
 
 void SetLocale(char *loc)
 {
-	my_locale = loc;
+    my_locale[4] = 0;
+    if(strlen(loc))
+    {
+	    memcpy(my_locale,loc,4);
+    }
+    else
+    {
+        uint32 fs = GetFileSize(cconf);
+        std::fstream fh;
+        std::string s;
+        fh.open(cconf,std::ios_base::in);
+        if(!fh.is_open())
+        {
+            printf("ERROR: unable to detect locale, could not open '%s'\n",cconf);
+            return;
+        }
+        char *buf = new char[fs];
+        fh.read((char*)buf,fs);
+        fh.close();
+        
+        for(uint32 i=0; i<fs; i++)
+        {
+            if(buf[i]=='\n')
+            {
+                if(s.length() >= strlen(cconfentry) && !memcmp(s.c_str(),cconfentry,strlen(cconfentry)))
+                {
+                    memcpy(my_locale,s.c_str() + strlen(cconfentry), 4);
+                    printf("Auto-detected locale '%s'\n",my_locale);
+                    break;
+                }
+            s.clear();
+            }
+            else
+            {
+                s += buf[i];
+            }
+        }
+        delete [] buf;
+    }
 }
 
 char *GetLocale(void)
 {
-	return my_locale;
+	return &my_locale[0];
 }
