5a02ff48e30219e6b9ecfbdef6a675c31d44635d
False.Genesis <False.Genesis@fda9a0ab-4e26-0410-ba80-894f06a4b443>
2007-10-17 13:44:07 +0000

    * now its version 13.31 (i forgot to set 13.3 before, nvm that. head -> keyboard)

--- src/Client/PseuWoW.cpp
+++ src/Client/PseuWoW.cpp
@@ -46,7 +46,7 @@ void PseuInstanceRunnable::sleep(uint32 msecs)
 PseuInstance::PseuInstance(PseuInstanceRunnable *run)
 {
     _runnable=run;
-    _ver="PseuWoW Alpha Build 13.2" DEBUG_APPENDIX;
+    _ver="PseuWoW Alpha Build 13.31" DEBUG_APPENDIX;
     _ver_short="A13.2" DEBUG_APPENDIX;
     _wsession=NULL;
     _rsession=NULL;
